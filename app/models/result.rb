12650d28e9f37cbb525321aec5f538c6237cf999
Reese McLean <reese.mclean@me.com>
2016-06-06 23:40:51 -0700

    Delete result now cascades result participants.

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,4 +1,4 @@
 class Result < ApplicationRecord
   belongs_to :league, inverse_of: :results
-  has_many :result_participants, inverse_of: :result
+  has_many :result_participants, dependent: :destroy, inverse_of: :result
 end
